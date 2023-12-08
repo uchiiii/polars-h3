1c10c654a9112d8a02435e81bf1ea156da42f4de
uchiiii <uchikun17@gmail.com>
2023-12-08 11:50:13 +0900

    Fix

--- polars_h3/polars_h3/src/lib.rs
+++ polars_h3/polars_h3/src/lib.rs
@@ -2,8 +2,8 @@ use h3o::{LatLng, Resolution};
 use polars::prelude::*;
 use pyo3_polars::derive::polars_expr;
 
-fn _geo_to_h3(lat: f64, lag: f64, resolution: Resolution) -> u64 {
-    let coord = LatLng::new(lat, lag).expect("invalid coord");
+fn _geo_to_h3(lat: f64, lng: f64, resolution: Resolution) -> u64 {
+    let coord = LatLng::new(lat, lng).expect("invalid coord");
     coord
         .to_cell(resolution)
         .try_into()
@@ -14,8 +14,8 @@ fn _geo_to_h3(lat: f64, lag: f64, resolution: Resolution) -> u64 {
 pub fn geo_to_h3(inputs: &[Series]) -> PolarsResult<Series> {
     let resolution: u8 = 3;
     let ca_lat = inputs[0].f64()?;
-    let ca_lag = inputs[1].f64()?;
-    let out: UInt64Chunked = arity::binary_elementwise_values(ca_lat, ca_lag, |a, b| {
+    let ca_lng = inputs[1].f64()?;
+    let out: UInt64Chunked = arity::binary_elementwise_values(ca_lat, ca_lng, |a, b| {
         _geo_to_h3(a, b, resolution.try_into().expect("hoge"))
     });
     Ok(out.into_series())
--- polars_h3/run.py
+++ polars_h3/run.py
@@ -2,11 +2,11 @@ import polars as pl
 from polars_h3 import H3
 
 
-data = {"lat": [34.6432], "lag": [134.9976]}
-df = pl.DataFrame(data, schema=[("lat", pl.Float64), ("lag", pl.Float64)])
+data = {"lat": [34.6432], "lng": [134.9976]}
+df = pl.DataFrame(data, schema=[("lat", pl.Float64), ("lng", pl.Float64)])
 
 print(
     df.with_columns(
-        h3=pl.col("lat").h3.geo_to_h3("lag"),
+        h3=pl.col("lat").h3.geo_to_h3("lng"),
     )
 )
